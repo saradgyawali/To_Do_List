2a2415d77df00adb4b4eec37666f8ebdb6d05acd
Sarad Gyawali <Saradgyawali@Sarads-MacBook-Pro.local>
2015-07-01 06:46:36 -0700

    second commit

--- script.js
+++ script.js
@@ -1,62 +1,28 @@
-/* $(function() {
-//toDos array (it holds our data)
-	var toDos = [
-	{name: "Cooking", desc: "Cook Pasta"},
-	{name: "Cleaning", desc: "Vaccum Living Room"},
-	{name: "Coding", desc: "Do HomeWork"}
+$(function() {
 
-  ];
-
+  // form to create new todo
   var $newToDo = $('#new-todo');
 
+  // element to hold our list of todos
   var $toDoList = $('#todo-list');
 
-
-  _.each(toDos, function (todo, index) {
-  	$toDoList.append('<li class="todo">' + todo.name + ' - ' + todo.desc + '</li>');
-
-  });
-
-  $newToDo.on('submit', function(event) {
-  	event.preventDefault();
-
-  	var toDoName = $('#todo-name').val();
-  	var toDoDesc = $('#todo-desc').val();
-  	var toDOData = {name: toDoName, desc: toDoDesc};
-
-  	$toDoList.append('<li class="todo>' + toDoData.name + ' - ' + toDoData.desc + '</li>');
-
-  	$newToDo[0].reset();
-  	$('#todo-name').focus();
-
-  });
-
-  $toDOList.on('click', '.todo', function() {
-  	$(this).addClass('done');
-
-  });
-}); */
-
-$(function() {
+  // todo template
+  var toDoTemplate = _.template($('#todo-template').html());
 
   // `toDos` array is our model (holds our data)
   // contains test (or "seed") data
   var toDos = [
-	{name: "Cooking", desc: "Cook Pasta"},
-	{name: "Cleaning", desc: "Vaccum Living Room"},
-	{name: "Coding", desc: "Do HomeWork"}
+    {name: "laundry", desc: "clean clothes"},
+    {name: "grocery shopping", desc: "buy food"},
+    {name: "nap time", desc: "remember to sleep!"}
   ];
-
-  // form to create new todo
-  var $newToDo = $('#new-todo');
-  
-  // element to hold our list of todos
-  var $toDoList = $('#todo-list');
 
   // append existing todos (from seed data) to `$toDoList`
   // `_.each` is an "iterator" function provided by Underscore.js
   _.each(toDos, function (todo, index) {
-    $toDoList.append('<li class="todo">' + todo.name + ' - ' + todo.desc + '</li>');
+    var $todo = $(toDoTemplate(todo));
+    $todo.attr('data-index', index);
+    $toDoList.append($todo);
   });
 
   // submit form to create new todo
@@ -70,9 +36,13 @@ $(function() {
 
     // store our new todo
     toDos.push(toDoData);
+    console.log(toDos);
+    var index = toDos.indexOf(toDoData);
 
     // append our new todo to the page
-    $toDoList.append('<li class="todo">' + toDoData.name + ' - ' + toDoData.desc + '</li>');
+    var $todo = $(toDoTemplate(toDoData));
+    $todo.attr('data-index', index);
+    $toDoList.append($todo);
 
     // reset the form
     $newToDo[0].reset();
@@ -80,8 +50,27 @@ $(function() {
   });
 
   // add class to todo on click to mark it as done
-  $toDoList.on('click', '.todo', function() {
-    $(this).addClass('done');
+  $toDoList.on('click', '.todo-text', function() {
+    $(this).toggleClass('done');
+  });
+
+  // remove todo from model and view
+  $toDoList.on("click", ".delete", function () {
+    var $todo = $(this).closest(".todo");
+    var index = $todo.attr('data-index');
+
+    // remove todo from the `toDos` array (model)
+    toDos.splice(index, 1);
+    console.log(toDos);
+
+    // remove todo from the DOM (view)
+    $todo.remove();
+
+    // reset indexes in DOM to match `toDos` array
+    // $.each loops through DOM elements
+    $('.todo').each(function(index) {
+      $(this).attr('data-index', index);
+    });
   });
 
 });
